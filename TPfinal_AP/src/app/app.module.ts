ce1046c7e17e996798f433d22eb0bc12890f44ab
Federico18011980 <federicorodriguez18@hotmail.com>
2022-09-20 20:06:52 -0300

    primeros componentes
    cargando primeros componentes

---
+++ TPfinal_AP/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { LogoComponent } from './componentes/logo/logo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LogoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
